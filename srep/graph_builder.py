9e9911970cf4e45116baa71e14cb0832a936bc1c
Arulmozhivarman Velusamy <arulx06@gmail.com>
2026-06-19 16:32:16 +0530

    graph builder skeleton changed

--- srep/graph_builder.py
+++ srep/graph_builder.py
@@ -5,6 +5,13 @@ import networkx as nx
 
 def build_graph():
     """Create a simple detection -> triage -> response graph."""
-    graph = nx.DiGraph()
-    graph.add_edges_from([("Detection", "Triage"), ("Triage", "Response")])
-    return graph
+    G = nx.DiGraph()
+
+    G.add_node("Detection")
+    G.add_node("Triage")
+    G.add_node("Response")
+
+    G.add_edge("Detection", "Triage")
+    G.add_edge("Triage", "Response")
+
+    return G
